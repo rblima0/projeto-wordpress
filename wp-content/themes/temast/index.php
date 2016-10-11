286362a1830161b89d608456ede022346c08c899
Rodrigo Banci <rblima0@gmail.com>
2016-10-11 18:31:03 -0300

    footer, header e index

--- wp-content/themes/temast/index.php
+++ wp-content/themes/temast/index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Temast</title>
-</head>
-<body>
+<?php get_header(); ?>
    
-   <h2>Seja bem vindo ao</h2>
-   <h1>TEMAST</h1>
-    
-</body>
-</html>
+<h2>Bem Vindo</h2>
+
+<?php get_footer(); ?>
