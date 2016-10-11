92e97c72c65f57c9ac4adc382715c3f9fce96d21
Rodrigo Banci <rblima0@gmail.com>
2016-10-11 17:27:15 -0300

    inicio do tema

---
+++ wp-content/themes/temast/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Temast</title>
+</head>
+<body>
+   
+   <h2>Seja bem vindo ao</h2>
+   <h1>TEMAST</h1>
+    
+</body>
+</html>
